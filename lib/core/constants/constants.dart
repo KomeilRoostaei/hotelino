04cc7e9e86610687591b473634cbd0c6bf75451e
KomeilRoostaei <komeylnazanin@gmail.com>
2025-08-01 22:59:28 +0330

    Add Constants and Color

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const String hotelData = "assets/data/hotel.json";
+  static const String baceUrlImage =
+      "https://dunijet.ir/content/projects/hotelino/";
+}
+
+class AppColors {
+  static const Color primary = Color(0xffB27258);
+
+  // light Theme Color :
+  static const Color lightText = Colors.black;
+  static const Color lightHint = Colors.grey;
+  static const Color lightInputFill = Color(0xffF5F5F5);
+  static const Color lightBorder = Colors.grey;
+  static const Color lightFocusedBorder = Colors.brown;
+  // Dark Theme :
+  static const Color darkText = Colors.white;
+  static const Color darkHint = Colors.grey;
+  static const Color darkInputFill = Color(0xff303030);
+  static const Color darkBorder = Color(0xff707070);
+  static const Color darkFocusedBorder = Colors.brown;
+  static const Color darkButton = Color(0xff5d4037);
+}
